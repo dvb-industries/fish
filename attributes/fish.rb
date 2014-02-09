0218d1c9c930390088938efddeef6b2e4a8cc56d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-09 10:43:02 +0100

    create a fish recipe

---
+++ attributes/fish.rb
@@ -0,0 +1,2 @@
+set_unless[:fish][:user] = "dvberkel"
+set_unless[:fish][:group] = "dvberkel"
---
+++ recipes/default.rb
@@ -0,0 +1,17 @@
+execute "add fish-shell/release-2 PPA" do
+  command "add-apt-repository ppa:fish-shell/release-2"
+end
+
+execute "update apt" do
+  command "apt-get update"
+end
+
+execute "install emacs" do
+  command "apt-get -y install fish"
+end
+
+execute "set fish as default" do
+  command "chsh -s /usr/bin/fish"
+  user node[:fish][:user]
+  group node[:fish][:group]
+end
